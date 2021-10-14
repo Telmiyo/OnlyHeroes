b9825cedfc6793d3be3bf3b45c073cb04b5b7b08
Telmiyo <telmoberoiz@hotmail.com>
2021-10-14 20:58:59 +0200

    Simple Commands

--- src/main/java/me/godnitze/onlyheroes/OnlyHeroes.java
+++ src/main/java/me/godnitze/onlyheroes/OnlyHeroes.java
@@ -1,5 +1,6 @@
 package me.godnitze.onlyheroes;
 
+import me.godnitze.onlyheroes.Commands.JoinCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OnlyHeroes extends JavaPlugin {
@@ -8,6 +9,10 @@ public final class OnlyHeroes extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
     System.out.print("OnlyHeroes initialized correctly");
+
+        //SetCommands
+    getCommand("OnlyHeroes").setExecutor(new JoinCommand());
+
     }
 
     @Override
---
+++ src/main/java/me/godnitze/onlyheroes/utils/Globals.java
@@ -0,0 +1,9 @@
+package me.godnitze.onlyheroes.utils;
+
+import org.bukkit.ChatColor;
+
+public class Globals {
+
+    public String OnlyHeroes = ChatColor.WHITE + "[" + ChatColor.GOLD + "OnlyHeroes" + ChatColor.WHITE + "]";
+
+}
